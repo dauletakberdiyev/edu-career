29388c267e9f1b4cf904c782fcc60b61d73a050b
YergalymS <78316319+YergalymS@users.noreply.github.com>
2021-11-19 16:55:31 +0600

    created vacancy model and migration

---
+++ database/migrations/2021_11_18_141228_create_vacancies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVacanciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacancy', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->integer('number of seats');
+            $table->integer('number of applied students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacancies');
+    }
+}
