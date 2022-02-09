5e7ad14e3f2b6791665d3c3598c1c70114e3d739
abylayomar <abylay.omar@gmail.com>
2022-02-09 13:21:31 +0600

    add role rand

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+
 use App\Models\User;
 
 class UserSeeder extends Seeder
@@ -15,6 +18,18 @@ class UserSeeder extends Seeder
     public function run()
     {
         //User::factory(100)->create();
+        $roles = Role::all('name')->toArray();
+        for ($i = 0; $i < 100; $i++) {
+            $user = User::create([
+                'name' => 'user ' . $i,
+                'email' => 'email' . $i . '@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]);
+            
+    
+            $user->assignRole($roles[$i % 4]['name']);
+        }
         $company = User::create([
             'name' => 'gg',
             'email' => '$this->faker->unique()->safeEmail()',
