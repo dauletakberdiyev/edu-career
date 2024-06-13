a8c21bddd44eb27d7620d5bbad13cfecec08b1e8
Abylay Omar <abylay.omar@gmail.com>
2024-06-13 11:43:17 +0500

    add cv template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -111,6 +111,11 @@
           </select>
         </div>
 
+
+        <div class="form-group mb-3">
+          <a href="{{ asset('docs/cv_template.pdf') }}" target='_blank' class="">Download CV template</a>
+        </div>
+
         <div class="form-group mb-3 hidden" id="student_cv">
           <label>Upload your CV</label>
           <input type="file" name="cv" accept="pdf,doc,docx" class="form-control-file">
